71c2a306951a7b577611dfe0b3af35f5244f151c
Aaron <aaronxyzheng@gmail.com>
2025-08-05 11:29:38 +0800

    add Main.java user interface code

--- src/main/java/com/aaronxyzheng/calculatorparser/Main.java
+++ src/main/java/com/aaronxyzheng/calculatorparser/Main.java
@@ -1,6 +1,30 @@
-package src.main.java.com.aaronxyzheng.calculatorparser;
+package com.aaronxyzheng.calculatorparser;
+
+import java.util.Scanner;
+
 public class Main {
+
+    // Method Variables
+    public static Scanner scanner = new Scanner(System.in);
+    public static MathLogic mathLogic = new MathLogic();
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        intro();
+        while(true) {
+            run();
+        }
+    }
+
+    public static void intro() {
+        System.out.println("           Welcome to Calculator Parser!");
+        System.out.println("Enter mathematical expressions using: + - * / ^ ( )");
+        System.out.println();
+    }
+
+    public static void run() {
+
+        System.out.print("Expression: ");
+        String userExpression = scanner.nextLine();
+        System.out.println("Answer: " + mathLogic.solveExpression(userExpression));
+        System.out.println();
     }
 }
---
+++ src/main/java/com/aaronxyzheng/calculatorparser/MathLogic.java
@@ -0,0 +1,7 @@
+package com.aaronxyzheng.calculatorparser;
+
+public class MathLogic {
+    public int solveExpression(String userExpression) {
+        return 1;
+    }
+}
