2089c3c097d1b65045102ba8b2721126f9a78a90
Aaron <aaronxyzheng@gmail.com>
2025-08-05 17:06:45 +0800

    chore: scaffold MathLogic methods without implementation

--- src/main/java/com/aaronxyzheng/calculatorparser/MathLogic.java
+++ src/main/java/com/aaronxyzheng/calculatorparser/MathLogic.java
@@ -1,7 +1,26 @@
 package com.aaronxyzheng.calculatorparser;
 
+import com.aaronxyzheng.calculatorparser.datastructures.*;
+import java.util.ArrayList;
+
 public class MathLogic {
     public int solveExpression(String userExpression) {
+        ArrayList<String> tokenList = tokenizer(userExpression);
+        Queue<String> postfixQueue = infixToPostfix(tokenList);
+        int answer = postfixEvaluater(postfixQueue);
+        return answer;
+    }
+
+    public ArrayList<String> tokenizer(String userExpression) {
+        return new ArrayList<>();
+    }
+
+    public Queue<String> infixToPostfix(ArrayList<String> infixArray) {
+        return new Queue<String>();
+    }
+
+    public int postfixEvaluater(Queue<String> postfixQueue) {
         return 1;
     }
+    
 }
